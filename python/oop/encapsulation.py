6ab35542ceed27d1646836b58d52f9ffc2c36a05
Binshabs <binshabs567@gmail.com>
2023-11-03 08:57:59 +0530

    file added

---
+++ python/oop/encapsulation.py
@@ -0,0 +1,22 @@
+# class A:
+#     _a=10
+#     def _disp(self):
+#         print(self._a)
+# obj=A()
+# obj._disp()
+# print(obj._a)
+
+
+
+
+# class bank:
+#     def __init__(self):
+#         self.accno=906879867
+#         self.savings=4000
+#     def __deposit(self,dep):
+#         self.savings+=dep
+#     def display(self):
+#         print("savings=",self.savings)
+# binsha=bank()
+# binsha.__deposit(4000)
+# binsha.display()
